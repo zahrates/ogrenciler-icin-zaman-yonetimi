10c6845231cd9bfe35a62700d6927bb7e2de3c97
zahrates <atesgamze29@gmail.com>
2026-06-21 21:32:08 +0300

    Proje güncellendi: JS animasyon + pomodoro + tema sistemi

--- script.js
+++ script.js
@@ -12,3 +12,110 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll(".fade-up").forEach((element) => {
   observer.observe(element);
 });
+// Sayfa yüklendiğinde scroll'u yukarı sabitle böylece her yenilendiğinde aşağı kaymasın
+window.addEventListener('load', () => {
+  window.scrollTo(0, 0);
+});
+/*
+// Navbar scroll animasyonu
+let lastScrollTop = 0;
+const navbar = document.querySelector('.navbar');
+*/
+window.addEventListener('scroll', () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (scrollTop > lastScrollTop) {
+    // Aşağı scroll'lanıyor → Navbar gizle
+    navbar.style.transform = 'translateY(-100%)';
+  } else {
+    // Yukarı scroll'lanıyor → Navbar göster
+    navbar.style.transform = 'translateY(0)';
+  }
+  
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
+
+// Theme Toggle
+const toggle = document.getElementById('darkModeToggle');
+const root = document.documentElement;
+const html = document.querySelector('html');
+
+// Sayfa yüklendiğinde hafızayı kontrol et
+const savedTheme = localStorage.getItem('theme-preference');
+
+if (savedTheme === 'dark') {
+  html.classList.add('dark-mode');
+  toggle.checked = true;
+} else if (savedTheme === 'light') {
+  html.classList.remove('dark-mode');
+  toggle.checked = false;
+} else {
+  // Sistem tercihini kullan
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    html.classList.add('dark-mode');
+    toggle.checked = true;
+  }
+}
+
+// Butona basıldığında değişikliği yap ve kaydet
+toggle.addEventListener('change', function () {
+  if (this.checked) {
+    html.classList.add('dark-mode');
+    localStorage.setItem('theme-preference', 'dark');
+    root.style.colorScheme = 'dark';
+  } else {
+    html.classList.remove('dark-mode');
+    localStorage.setItem('theme-preference', 'light');
+    root.style.colorScheme = 'light';
+  }
+});
+
+
+/*pomodoro    Ü<ERİNDEN TEKRAR GEÇ */
+let sure = 25 * 60;
+let sayac;
+
+function guncelle() {
+
+    const dakika = Math.floor(sure / 60);
+    const saniye = sure % 60;
+
+    document.getElementById("timer").textContent =
+        `${dakika.toString().padStart(2,"0")}:${saniye.toString().padStart(2,"0")}`;
+}
+
+function baslat(){
+
+    if(sayac) return;
+
+    sayac = setInterval(() => {
+
+        if(sure > 0){
+            sure--;
+            guncelle();
+        }
+        else{
+            clearInterval(sayac);
+            sayac = null;
+            alert("Pomodoro tamamlandı 🎉");
+        }
+
+    },1000);
+}
+
+function duraklat(){
+    clearInterval(sayac);
+    sayac = null;
+}
+
+function sifirla(){
+
+    clearInterval(sayac);
+    sayac = null;
+
+    sure = 25 * 60;
+
+    guncelle();
+}
+
+guncelle();
